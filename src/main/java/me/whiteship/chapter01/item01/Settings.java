441fe5755557cc8c95cceae91284f39c6919fd41
ChanJunHa <hachanzzzz@gmail.com>
2025-05-12 22:38:25 +0900

    test

--- src/main/java/me/whiteship/chapter01/item01/Settings.java
+++ src/main/java/me/whiteship/chapter01/item01/Settings.java
@@ -3,6 +3,9 @@ package me.whiteship.chapter01.item01;
 /**
  * 이 클래스의 인스턴스는 #getInstance()를 통해 사용한다.
  * @see #getInstance()
+ * 
+ * 자바의 생성자는 호출될 때 마다 새로운 객체를 만든다.
+ * 
  */
 public class Settings {
 
